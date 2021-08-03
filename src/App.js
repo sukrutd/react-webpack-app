a0a2e97b60e31fee2b3dbc5c499d4b64dcf56b9b
sukrutd <sukrut.dd@gmail.com>
2021-08-03 13:22:08 +0530

    Added vscode settings for eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-    return <div>React App</div>;
+    return (
+        <div>
+            React App <button type='button'>Learn React</button>
+        </div>
+    );
 };
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders a message', () => {
     render(<App />);
     expect(screen.getByText(/react app/i)).toBeInTheDocument();
 });
+
+test('button renders correctly', () => {
+    render(<App />);
+    const button = screen.getByRole('button');
+    expect(button).toHaveTextContent('Learn React');
+    expect(button).toBeEnabled();
+});
